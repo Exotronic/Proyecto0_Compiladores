8e52e52ab7ca666b5c7eacbd797d506c5dc99935
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2021-04-11 19:13:49 -0600

    Add files via upload

--- other/semantics.c
+++ other/semantics.c
@@ -303,9 +303,9 @@ expr_rec gen_infix(expr_rec e1, op_rec op, expr_rec e2) {
 
 // Escribe la operacion para leer de stdin.
 void read_id(expr_rec in_var) {
-	fprintf(output, "\tldr r0, =");
-	fprintf(output, extract(in_var));
-	fprintf(output, "\n\tbl scanf\n");
+	fprintf(output, "\n\tldr r0, =input\n");
+	//fprintf(output, extract(in_var));
+	fprintf(output, "\tbl scanf\n");
 }
 
 expr_rec process_id(char *token) {
